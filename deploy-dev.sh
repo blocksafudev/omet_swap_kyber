cb5fbc1c2c7137150d66973ef5336baad61a5c34
Blocksafu Dev <blocksafudev@gmail.com>
2025-04-09 18:56:40 +0700

    Update deploy-dev.sh

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -4,4 +4,4 @@ docker build -t omet-swap-kyber .
 docker stop omet-swap-kyber || true
 docker container rm omet-swap-kyber || true
 docker rmi -f $(docker images -f "dangling=true" -q) || true
-docker run -d --name=omet-swap-kyber --network=default-net --restart unless-stopped -p 8080:80 omet-swap-kyber
+docker run -d --name=omet-swap-kyber --network=default-net --restart unless-stopped -p 8080:3000 omet-swap-kyber
